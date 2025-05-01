a20cebb5f407259639c8d9d8a7fe24cc759b15fe
vashujoshi <118829508+vashujoshi@users.noreply.github.com>
2025-05-01 22:10:59 +0530

    updated

--- server/routes/otp_routes.js
+++ server/routes/otp_routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { sendOtp, verifyOtp } = require('../controllers/Otp_controller');
+const { sendOtp, verifyOtp } = require('../controllers/otp_controller');
 
 router.post('/send-otp', sendOtp);
 router.post('/verify-otp', verifyOtp);
